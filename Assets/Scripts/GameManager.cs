ccf1562521671402c5f8398219b343297c15cbe0
BrettFX <brettallen777@gmail.com>
2020-11-03 23:24:50 -0500

    Implement initialization for stats Next steps are to begin implementing the calculations for on winning and on losing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -404,16 +404,7 @@ namespace Solitaire
         **/
         private void UpdateTimer()
         {
-            float t = m_stopWatch.ElapsedMilliseconds / 1000.0f;
-            float milliseconds = (Mathf.Floor(t * 100) % 100); // calculate the milliseconds for the timer
-
-            int seconds = (int)(t % 60); // return the remainder of the seconds divide by 60 as an int
-            t /= 60; // divide current time y 60 to get minutes
-            int minutes = (int)(t % 60); //return the remainder of the minutes divide by 60 as an int
-            t /= 60; // divide by 60 to get hours
-            int hours = (int)(t % 24); // return the remainder of the hours divided by 60 as an int
-
-            lblTimer.text = string.Format("{0}:{1}:{2}.{3}", hours.ToString("00"), minutes.ToString("00"), seconds.ToString("00"), milliseconds.ToString("00"));
+            lblTimer.text = Utils.GetTimestamp(m_stopWatch.ElapsedMilliseconds);
         }
 
         /**
--- Assets/Scripts/StatsManager.cs
+++ Assets/Scripts/StatsManager.cs
@@ -23,17 +23,28 @@ namespace Solitaire
     {
         public static StatsManager Instance { get; private set; }
 
+        private const string FASTEST_TIME_KEY = "FastestTime";
+        private const string LONGEST_TIME_KEY = "LongestTime";
+        private const string AVERAGE_TIME_KEY = "AverageTime";
+        private const string LEAST_MOVES_KEY = "LeastMoves";
+        private const string MOST_MOVES_KEY = "MostMoves";
+        private const string AVERAGE_MOVES_KEY = "AverageMoves";
+        private const string TOTAL_MOVES_KEY = "TotalMoves";
+        private const string TOTAL_WINS_KEY = "TotalWins";
+        private const string TOTAL_LOSSES_KEY = "TotalLosses";
+        private const string WIN_LOSS_RATIO_KEY = "WinLossRatio";
+
         [Header("Statistics Containers")]
-        public GameObject fastestTimeStat;
-        public GameObject longestTimeStat;
-        public GameObject averageTimeStat;
-        public GameObject leastMovesStat;
-        public GameObject mostMovesStat;
-        public GameObject averageMovesStat;
-        public GameObject totalMovesStat;
-        public GameObject totalWinsStat;
-        public GameObject totalLossesStat;
-        public GameObject winLossRatioStat;
+        public Statistic fastestTimeStat;
+        public Statistic longestTimeStat;
+        public Statistic averageTimeStat;
+        public Statistic leastMovesStat;
+        public Statistic mostMovesStat;
+        public Statistic averageMovesStat;
+        public Statistic totalMovesStat;
+        public Statistic totalWinsStat;
+        public Statistic totalLossesStat;
+        public Statistic winLossRatioStat;
 
         private float m_fastestTimeMillis;
         private float m_longestTimeMillis;
@@ -44,7 +55,7 @@ namespace Solitaire
         private ulong m_totalMoves;
         private ulong m_totalWins;
         private ulong m_totalLoss;
-        private float winLossRatio;
+        private float m_winLossRatio;
 
         private void Awake()
         {
@@ -70,13 +81,46 @@ namespace Solitaire
         // Start is called before the first frame update
         void Start()
         {
+            // Load everything from PlayerPrefs to the respective stat
+            m_fastestTimeMillis = PlayerPrefs.GetFloat(FASTEST_TIME_KEY, 0.0f);
+            DisplayStatistic(fastestTimeStat, m_fastestTimeMillis.ToString(), true);
+
+            m_longestTimeMillis = PlayerPrefs.GetFloat(LONGEST_TIME_KEY, 0.0f);
+            DisplayStatistic(longestTimeStat, m_longestTimeMillis.ToString(), true);
+
+            m_averageTimeMillis = PlayerPrefs.GetFloat(AVERAGE_TIME_KEY, 0.0f);
+            DisplayStatistic(averageTimeStat, m_averageTimeMillis.ToString(), true);
+
+            m_leastMoves = (uint)PlayerPrefs.GetInt(LEAST_MOVES_KEY, 0);
+            DisplayStatistic(leastMovesStat, m_leastMoves.ToString(), false);
+
+            m_mostMoves = (uint)PlayerPrefs.GetInt(MOST_MOVES_KEY, 0);
+            DisplayStatistic(mostMovesStat, m_mostMoves.ToString(), false);
+
+            m_averageMoves = PlayerPrefs.GetFloat(AVERAGE_MOVES_KEY, 0.0f);
+            DisplayStatistic(averageMovesStat, m_averageMoves.ToString(), false);
+
+            m_totalMoves = ulong.Parse(PlayerPrefs.GetString(TOTAL_MOVES_KEY, "0"));
+            DisplayStatistic(totalMovesStat, m_totalMoves.ToString(), false);
+
+            m_totalWins = ulong.Parse(PlayerPrefs.GetString(TOTAL_WINS_KEY, "0"));
+            DisplayStatistic(totalWinsStat, m_totalWins.ToString(), false);
+
+            m_totalLoss = ulong.Parse(PlayerPrefs.GetString(TOTAL_LOSSES_KEY, "0"));
+            DisplayStatistic(totalLossesStat, m_totalLoss.ToString(), false);
+
+            m_winLossRatio = PlayerPrefs.GetFloat(WIN_LOSS_RATIO_KEY, 0.0f);
+            DisplayStatistic(winLossRatioStat, m_winLossRatio.ToString(), false);
 
         }
 
-        // Update is called once per frame
-        void Update()
+        /**
+         * 
+         */
+        private void DisplayStatistic(Statistic statistic, string value, bool isTimestamp)
         {
-
+            string formatted = isTimestamp ? Utils.GetTimestamp(float.Parse(value)) : string.Format(value);
+            statistic.SetText(formatted);
         }
 
         /**
@@ -86,6 +130,7 @@ namespace Solitaire
         public void OnWin()
         {
             Debug.Log("Game has been won. Saving statistics (not yet implemented)...");
+            // TODO calculate all winning related stats here
         }
 
         /**
@@ -95,6 +140,9 @@ namespace Solitaire
         public void OnLose()
         {
             Debug.Log("Game has been lost. Saving statistics (not yet implemented)...");
+            // TODO calculate all losing related stats here
+            m_totalLoss++;
+
         }
 
         /**
