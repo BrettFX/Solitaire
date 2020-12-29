5c909a791ce50e5d8abba4008fb6f14084d6c432
BrettFX <brettallen777@gmail.com>
2020-12-29 13:38:03 -0500

    Implement ability to detect screen orientation changes Dynamically setting canvas scaler reference resolution based on detected screen orientation (on change).
    Currently working on implementing spam guard to limit the amount of
    screen orientation changes within a specific amount of time.

--- Solitaire/Assets/Scripts/Managers/OrientationManager.cs
+++ Solitaire/Assets/Scripts/Managers/OrientationManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Solitaire
 {
@@ -7,9 +8,20 @@ namespace Solitaire
     {
         public static OrientationManager Instance { get; private set; }
 
-        private RectTransform m_rectTransform;
+        // Threshold time in milliseconds allowed between screen orientation change events
+        private const float ORIENTATION_CHANGE_TIME_THRESHOLD = 250.0f;
+
+        [Header("References")]
         public GameObject landscape;
         public GameObject portrait;
+        public CanvasScaler scaler;
+
+        [Header("Resolution Configuration")]
+        public Vector2 portraitRes;
+        public Vector2 landscapeRes;
+
+        private RectTransform m_rectTransform;
+        private System.Diagnostics.Stopwatch m_stopwatch;
 
         private void Awake()
         {
@@ -32,23 +44,56 @@ namespace Solitaire
             Instance = GetComponent<OrientationManager>();
 
             m_rectTransform = GetComponent<RectTransform>();
+
+            m_stopwatch = new System.Diagnostics.Stopwatch();
+            m_stopwatch.Start();
+
             SetOrientation();
         }
 
+        
+
         /**
-         * 
+         * Detect the new screen orientation and toggle the visibility of the 
+         * associated portrait or landscape game objects based on the new 
+         * screen orientation. Also, configure the CanvasScaler to conform to the
+         * new screen resolution.
          */
         private void SetOrientation()
         {
+            // TODO add spam guard with stopwatch (only allow orientation change events every n second(s))
+            if (m_stopwatch.ElapsedMilliseconds < ORIENTATION_CHANGE_TIME_THRESHOLD) return;
+
             if (m_rectTransform == null) return;
             bool verticalOrientation = m_rectTransform.rect.width < m_rectTransform.rect.height;
+
+            float newX = verticalOrientation ? portraitRes.x : landscapeRes.x;
+            float newY = verticalOrientation ? portraitRes.y : landscapeRes.y;
+            Vector2 newRes = new Vector2(newX, newY);
+
+            Debug.Log("Should set canvas scaler reference resolution to: " + newRes);
+
+            // Set the canvas scaler reference resolution based on new orientation
+            // scaler.referenceResolution.Set(newX, newY);
+            scaler.referenceResolution = newRes;
+
+            Debug.Log("Current canvas scaler reference resolution is: " + scaler.referenceResolution);
+
             portrait.SetActive(verticalOrientation);
             landscape.SetActive(!verticalOrientation);
+
+
         }
 
+        /**
+         * Event triggered by UnityEngine.EventSystems (UIBehaviour) on mobile
+         * devices when the screen orientation changes.
+         * 
+         * Implementation invokes a custom handle function for when the orientation
+         * changes to determine the new orientation and process accordingly.
+         */
         private void OnRectTransformDimensionsChange()
         {
-            Debug.Log("Detected orientation change.");
             SetOrientation();
         }
 
